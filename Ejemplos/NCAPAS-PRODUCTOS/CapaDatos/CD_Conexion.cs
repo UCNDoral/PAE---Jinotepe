7a2da978e119d217f47619ae2fdcdd59d726791f
Angel Larios <angel.larios@ucn.edu.ni>
2026-03-16 10:11:42 -0600

    se agrego el archivo de conexion a ala base de datos.

---
+++ Ejemplos/NCAPAS-PRODUCTOS/CapaDatos/CD_Conexion.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.SqlClient
+using System.Data;
+
+namespace CapaDatos
+{
+    internal class CD_Conexion
+    {
+        private SqlConnection Conexion = new SqlConnection("Server=localhost; Database=Prueba; Integrated Security=true");
+
+
+        public SqlConnection AbrirConexion()
+        {
+            if (Conexion.State == ConnectionState.Closed) Conexion.Open();
+
+            return Conexion;
+
+        }
+
+        public SqlConnection CerrarConexion()
+        {
+            if (Conexion.State == ConnectionState.Open) Conexion.Close();
+
+            return Conexion;
+
+        }
+
+    }
+}
